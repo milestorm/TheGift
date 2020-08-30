659ca5c8a3e9329899048b078c331791deb14272
Caivas, David <David.Caivas@firma.seznam.cz>
2020-08-30 15:39:11 +0200

    init

---
+++ include/config.h
@@ -0,0 +1,11 @@
+// PIN definitions
+
+#define PIN_R 3
+#define PIN_G 5
+#define PIN_B 6
+#define PIN_W 8
+
+#define PIN_IR 16
+
+#define PIN_SWITCH_1 15
+#define PIN_SWITCH_2 14
---
+++ src/main.cpp
@@ -0,0 +1,72 @@
+#include <Arduino.h>
+#include <avdweb_VirtualDelay.h>
+#include <OneButton.h>
+#include <config.h>
+
+// TODO
+// udelat dejchaci ledky do sluplicku
+// pri vlozeni udelat efekt bilyho svetlaa jakoze zarivka zablikani
+// pri vylozeni podobny ale kratsi
+
+VirtualDelay whiteDelay;
+
+OneButton switch_1(PIN_SWITCH_1, true);
+OneButton switch_2(PIN_SWITCH_2, true);
+
+unsigned long white_on_time = 1 * 60 * 1000; // one minute
+unsigned long white_on_time_previous = 0;
+bool turn_white_on = false;
+bool want_turn_white_on = false;
+
+bool ir_detected = false;
+
+bool detect_ir() {
+  bool ir_on = false;
+  if (digitalRead(PIN_IR) == HIGH) {
+    ir_on = true;
+  } else {
+    ir_on = false;
+  }
+
+  return ir_on;
+}
+
+void white_led_tick() {
+  if (want_turn_white_on == true) {
+    digitalWrite(PIN_W, HIGH);
+    whiteDelay.start(white_on_time);
+    if (whiteDelay.elapsed()) {
+        digitalWrite(PIN_W, LOW);
+        want_turn_white_on = false;
+    }
+  }
+}
+
+void setup() {
+  pinMode(PIN_R, OUTPUT);
+  pinMode(PIN_G, OUTPUT);
+  pinMode(PIN_B, OUTPUT);
+  pinMode(PIN_W, OUTPUT);
+
+  pinMode(PIN_IR, INPUT);
+
+  // initial turn on
+  digitalWrite(PIN_R, HIGH);
+  digitalWrite(PIN_G, LOW);
+}
+
+void loop() {
+  if ((detect_ir() == true) && (ir_detected == false)) {
+    ir_detected = true;
+    digitalWrite(PIN_R, LOW);
+    digitalWrite(PIN_G, HIGH);
+    want_turn_white_on = true;
+  }
+
+  if ((detect_ir() == false) && (ir_detected == true)) {
+    digitalWrite(PIN_R, HIGH);
+    digitalWrite(PIN_G, LOW);
+  }
+
+  white_led_tick();
+}
